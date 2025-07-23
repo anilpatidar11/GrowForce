bb966e48e3b535865ba957e1c89bfe74a86a1b2d
anilpatidar11 <anilpatidar656@gmail.com>
2025-07-24 01:10:30 +0530

    Updated: used .env for API URL in Login

--- server/server.js
+++ server/server.js
@@ -4,19 +4,32 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
- const userRouter = require("./routers/userRouter");
- const EmployeeRouter = require("./routers/employeeRouter")
+const userRouter = require("./routers/userRouter");
+const EmployeeRouter = require("./routers/employeeRouter");
 
 const app = express();
-app.use(cors());
+
+//  CORS setup using environment variable
+const allowedOrigins = [process.env.FRONTEND_URL];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 app.use(express.json());
 
+//  Routes
+app.use('/api', userRouter);
+app.use('/api', EmployeeRouter);
 
- app.use('/api', userRouter);
- app.use('/api', EmployeeRouter);
-
-
-
+//  MongoDB connection
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -24,9 +37,8 @@ mongoose.connect(process.env.MONGO_URL, {
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.error("MongoDB connection error:", err));
 
-
+//  Start server
 const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
-
